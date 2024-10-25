31ba7d724eec94f4d085f24795c990d751d71e76
Esteban-Lombar <esteban.castano01@unicatolica.edu.co>
2024-10-24 23:53:39 -0500

    cors de cualquier lugar

--- backendParcial/index.js
+++ backendParcial/index.js
@@ -7,21 +7,15 @@ const app = express();
 const db = require('./db/mongo');
 app.use(urlencoded({extended: true}))
 app.use(json())
+app.use(cors());
 
 db.dbInit().then(() => console.log('Conexion realizada'))
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://parcial2frontend.vercel.app");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
-
 app.use(cors({
-    origin: 'https://parcial2frontend.vercel.app'  // Reemplaza con la URL de tu frontend
-  }));
+    origin: '*',  // Permitir cualquier origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Permitir solo ciertos métodos si es necesario
+    allowedHeaders: ['Content-Type', 'Authorization']  // Permitir solo ciertos encabezados
+}));
 
 app.use('/auth', router);
 
