3ee13f614119ee980c6b1ef49c2bff1ddbc6db09
Esteban-Lombar <esteban.castano01@unicatolica.edu.co>
2024-10-25 17:52:12 -0500

    se intenta solucionar

--- backendParcial/index.js
+++ backendParcial/index.js
@@ -3,16 +3,14 @@ const { urlencoded, json } = require('express');
 const router = require('./routes/login.routes.js');
 const cors = require('cors');
 const app = express();
-
 const db = require('./db/mongo');
 
 // Habilita CORS con configuraciones específicas
-app.use(cors({
+app.use(cors({ 
     origin: '*',  // Permitir cualquier origen
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTION'],  // Métodos permitidos
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Métodos permitidos (fíjate en la corrección aquí)
     allowedHeaders: ['Content-Type', 'Authorization']  // Encabezados permitidos
 }));
-
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
@@ -22,7 +20,7 @@ db.dbInit()
         console.log('Conexión realizada');
         app.use('/auth', router); // Mueve esto dentro de la promesa
         app.listen(4000, () => {
-            console.log('listening at port 4000');
+            console.log('Servidor corriendo en puerto 4000');
         });
     })
     .catch(err => {
