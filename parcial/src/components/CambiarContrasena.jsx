32141919c3fcfe8379fa5d6b886fe9920e45c43f
Esteban-Lombar <esteban.castano01@unicatolica.edu.co>
2024-10-25 00:55:43 -0500

    se cambio el form2

--- parcial/src/components/CambiarContrasena.jsx
+++ parcial/src/components/CambiarContrasena.jsx
@@ -1,10 +1,7 @@
 import { useState } from "react";
 import '../components/styles/cambiarcontrasena.css'
 import { useState } from "react"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+
 
 const CambiarContrasena = () => {
   const [username, setUsername] = useState("");
