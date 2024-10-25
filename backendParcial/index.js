9cf30e159347cbfd2048aa65295fb7815cf247e5
Esteban-Lombar <esteban.castano01@unicatolica.edu.co>
2024-10-24 22:50:16 -0500

    quitar cors de backend

--- backendParcial/index.js
+++ backendParcial/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const {urlencoded, json} = require('express');
 const router = require('./routes/login.routes');
-const cors = require('cors');
 const app = express();
 
 const db = require('./db/mongo');
@@ -9,7 +8,6 @@ app.use(urlencoded({extended: true}))
 app.use(json())
 
 db.dbInit().then(() => console.log('Conexion realizada'))
-app.use(cors())
 app.use('/auth', router);
 
 app.listen(4000, ()=>{
